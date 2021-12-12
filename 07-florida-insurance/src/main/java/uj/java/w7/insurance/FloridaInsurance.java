d7bb10eab2caddd6d5d293bf9fa3426f0391250a
Dominik Marchewka <marchewkad09@gmail.com>
2021-12-12 18:05:41 +0100

    count and tiv2012 done

--- 07-florida-insurance/src/main/java/uj/java/w7/insurance/FloridaInsurance.java
+++ 07-florida-insurance/src/main/java/uj/java/w7/insurance/FloridaInsurance.java
@@ -2,6 +2,10 @@ package uj.java.w7.insurance;
 
 import java.io.IOException;
 import java.io.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -11,10 +15,32 @@ import java.util.zip.ZipEntry;
 
 public class FloridaInsurance {
 
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
     public static int countCountry(List<InsuranceEntry> list){
 
         List<String> countedCountries = new ArrayList<>();
         int count = 0;
+
+        File countFile = new File("count.txt");
+
+        try{
+            if(countFile.createNewFile()){
+                System.out.println("Created count file");
+            }else{
+                System.out.println("Count file already exists");
+            }
+        }catch (IOException e){
+            System.out.println("An error occured");
+        }
+
+        OutputStream out = null;
+
+        try{
+            out = new FileOutputStream(countFile);
+        }catch (IOException e){
+            System.out.println("OutputStream error");
+        }
 
         for (var entry: list) {
             if(countedCountries.isEmpty() || !countedCountries.contains(entry.county)){
@@ -24,6 +50,13 @@ public class FloridaInsurance {
             }
         }
 
+        try{
+            System.out.println("Writing to countFile");
+            out.write(Integer.toString(count).getBytes(StandardCharsets.UTF_8));
+        }catch (IOException e){
+            System.out.println("Writing error");
+        }
+
         return count;
     }
 
@@ -31,9 +64,38 @@ public class FloridaInsurance {
 
         double sum = 0;
 
+        File countFile = new File("tiv2012.txt");
+
+        try{
+            if(countFile.createNewFile()){
+                System.out.println("Created tiv2012 file");
+            }else{
+                System.out.println("Tiv2012 file already exists");
+            }
+        }catch (IOException e){
+            System.out.println("An error occured");
+        }
+
+        OutputStream out = null;
+
+        try{
+            out = new FileOutputStream(countFile);
+        }catch (IOException e){
+            System.out.println("OutputStream error");
+        }
+
         for (var entry: list) {
             //System.out.println(entry.tiv_2012);
             sum += Double.parseDouble(entry.tiv_2012);
+        }
+
+        try{
+            System.out.println("Writing to countFile");
+            out.write(df.format(sum).getBytes(StandardCharsets.UTF_8));
+            //out.write('\n');
+            //out.write(Double.toString(sum).getBytes(StandardCharsets.UTF_8));
+        }catch (IOException e){
+            System.out.println("Writing error");
         }
 
         return sum;
@@ -49,30 +111,89 @@ public class FloridaInsurance {
 
     public static void mostValuable(List<InsuranceEntry> list){
 
+        List<InsuranceEntry> sums = new ArrayList<>();
         List<InsuranceEntry> top10 = new ArrayList<>();
         InsuranceEntryComparator comp = new InsuranceEntryComparator();
+        InsuranceEntry minValue = null;
 
-        for (var entry: list ) {
+        File countFile = new File("most_valuable.txt");
 
-            if(top10.size() < 10) {
-                top10.add(entry);
-                top10.sort(comp);
-                deleteDuplicate(top10);
+        try{
+            if(countFile.createNewFile()){
+                System.out.println("Created most_valuable file");
+            }else{
+                System.out.println("most_valuable file already exists");
             }
-            else{
-                if(entry.tivDifference > top10.get(9).tivDifference) {
-                    top10.remove(9);
-                    top10.add(entry);
-                    top10.sort(comp);
-                    deleteDuplicate(top10);
+        }catch (IOException e){
+            System.out.println("An error occured");
+        }
+
+        OutputStream out = null;
+
+        try{
+            out = new FileOutputStream(countFile);
+        }catch (IOException e){
+            System.out.println("OutputStream error");
+        }
+
+        boolean added = false;
+
+        for (var entry:list) {
+
+            for(int i = 0; i < sums.size(); i++){
+                if(sums.get(i).county.matches(entry.county)){
+                    sums.get(i).tivDifference += entry.tivDifference;
+                    added = true;
                 }
             }
+            if(added == false){
+                sums.add(entry);
+            }
+
+            added = false;
+            /*if(top10.size() < 10){
+                top10.add(entry);
+            }
+            else{
+                if(entry.tivDifference > minValue.tivDifference){
+                    top10.remove(minValue);
+                    top10.add(entry);
+                }
+            }
+            deleteDuplicate(top10);
+            minValue = findMin(top10);*/
         }
 
-        for (var entry:top10) {
-            System.out.println(entry.county + " " + entry.tivDifference);
+        sums.sort(comp);
+
+        //top10.sort(comp);
+
+        try{
+            out.write("country,value\n".getBytes(StandardCharsets.UTF_8));
+        }catch (IOException e){}
+        for (int i = 0; i < 10; i++) {
+            try{
+                out.write(sums.get(i).county.getBytes(StandardCharsets.UTF_8));
+                out.write(",".getBytes(StandardCharsets.UTF_8));
+                out.write(df.format(sums.get(i).tivDifference).getBytes(StandardCharsets.UTF_8));
+                out.write("\n".getBytes(StandardCharsets.UTF_8));
+            }catch (IOException e){
+                System.out.println("Writing error");
+            }
         }
-        System.out.println("size" + top10.size());
+        //System.out.println("size" + top10.size());
+    }
+
+    public static InsuranceEntry findMin(List<InsuranceEntry> list){
+        InsuranceEntry min;
+
+        min = list.get(0);
+        for(int i = 1; i < list.size(); i++){
+            if(list.get(i).tivDifference < min.tivDifference)
+                min = list.get(i);
+        }
+
+        return min;
     }
 
     public static void deleteDuplicate(List<InsuranceEntry> list){
@@ -107,51 +228,36 @@ public class FloridaInsurance {
         ZipFile zipFile = null;
         ZipEntry zipEntry = null;
         InputStream inputStream = null;
-        Reader inputStreamReader = null;
-        int data = 0;
-        char c = 'a';
-        String line = "";
         int entries = 0;
         int added = 0;
+        String text = null;
 
         try{
             zipFile = new ZipFile("C:\\Users\\Admin\\Desktop\\Studia\\java\\dominikmarchewka\\07-florida-insurance\\FL_insurance.csv.zip");
             zipEntry = zipFile.getEntry("FL_insurance.csv");
             inputStream = zipFile.getInputStream(zipEntry);
-            inputStreamReader = new InputStreamReader(inputStream);
 
-            data = inputStreamReader.read();
-            c = (char) data;
+            text = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
 
         }catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-        while(data != -1){
-            line += c;
+        var textArray = text.split("\r", 0);
 
-            if(c == '\r'){
-                entries++;
-                if(line.split(",").length == 18){
-                    added++;
-                    entryList.add(new InsuranceEntry(line));
-                    //entryList.get(added++).DisplayEntry();
-                }
-                //System.out.println(line);
-                line = "";
-            }
+        //System.out.println(textArray[0].split("\n")[0]);
+        entryList.add(new InsuranceEntry(textArray[0].split("\n")[1]));
 
-            try{
-                data = inputStream.read();
-                c = (char) data;
-            }catch (IOException e){
-                System.out.println(e.getMessage());
-            }
+        for (int i = 1; i < textArray.length; i++) {
+            entries++;
+            added++;
+            entryList.add(new InsuranceEntry(textArray[i]));
         }
 
+
         System.out.println(entries + " " + added);
-        //System.out.println(countCountry(entryList));
-        //System.out.println(sumTiv2012(entryList));
+        System.out.println(countCountry(entryList));
+        System.out.println(sumTiv2012(entryList));
         mostValuable(entryList);
     }
 
