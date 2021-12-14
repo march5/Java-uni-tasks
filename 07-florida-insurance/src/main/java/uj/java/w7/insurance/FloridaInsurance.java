a52438deb6f4977467445bfaf12c1c7cbdcbee32
Dominik Marchewka <marchewkad09@gmail.com>
2021-12-14 19:20:19 +0100

    florida done

--- 07-florida-insurance/src/main/java/uj/java/w7/insurance/FloridaInsurance.java
+++ 07-florida-insurance/src/main/java/uj/java/w7/insurance/FloridaInsurance.java
@@ -19,10 +19,10 @@ public class FloridaInsurance {
 
     public static int countCountry(List<InsuranceEntry> list){
 
-        List<String> countedCountries = new ArrayList<>();
+        List<String> countedCountries = new ArrayList<>();//list with countries already counted
         int count = 0;
 
-        File countFile = new File("count.txt");
+        File countFile = new File("count.txt");//creating file
 
         try{
             if(countFile.createNewFile()){
@@ -37,14 +37,13 @@ public class FloridaInsurance {
         OutputStream out = null;
 
         try{
-            out = new FileOutputStream(countFile);
+            out = new FileOutputStream(countFile);//creating output stream
         }catch (IOException e){
             System.out.println("OutputStream error");
         }
 
         for (var entry: list) {
-            if(countedCountries.isEmpty() || !countedCountries.contains(entry.county)){
-                //entry.DisplayEntry();
+            if(countedCountries.isEmpty() || !countedCountries.contains(entry.county)){//counting the countries that have not been counted yet
                 count++;
                 countedCountries.add(entry.county);
             }
@@ -52,7 +51,7 @@ public class FloridaInsurance {
 
         try{
             System.out.println("Writing to countFile");
-            out.write(Integer.toString(count).getBytes(StandardCharsets.UTF_8));
+            out.write(Integer.toString(count).getBytes(StandardCharsets.UTF_8));//writing the amount of countries to the file
         }catch (IOException e){
             System.out.println("Writing error");
         }
@@ -85,15 +84,12 @@ public class FloridaInsurance {
         }
 
         for (var entry: list) {
-            //System.out.println(entry.tiv_2012);
-            sum += Double.parseDouble(entry.tiv_2012);
+            sum += Double.parseDouble(entry.tiv_2012);//counting the sum of tiv_2012
         }
 
         try{
             System.out.println("Writing to countFile");
-            out.write(df.format(sum).getBytes(StandardCharsets.UTF_8));
-            //out.write('\n');
-            //out.write(Double.toString(sum).getBytes(StandardCharsets.UTF_8));
+            out.write(df.format(sum).getBytes(StandardCharsets.UTF_8));//writing it to file
         }catch (IOException e){
             System.out.println("Writing error");
         }
@@ -101,21 +97,18 @@ public class FloridaInsurance {
         return sum;
     }
 
-    //compares entries by tiv2012 and tiv2011 difference value(most valuable)
+    //compares entries by tiv2012 and tiv2011 value difference (most valuable)
     public static class InsuranceEntryComparator implements Comparator<InsuranceEntry>{
 
         public int compare(InsuranceEntry first, InsuranceEntry second){
-            return -Float.compare(first.tivDifference, second.tivDifference);
+            return -first.tivDifference.compareTo(second.tivDifference);
         }
     }
 
     public static void mostValuable(List<InsuranceEntry> list){
 
         List<InsuranceEntry> sums = new ArrayList<>();
-        List<InsuranceEntry> top10 = new ArrayList<>();
         InsuranceEntryComparator comp = new InsuranceEntryComparator();
-        InsuranceEntry minValue = null;
-
         File countFile = new File("most_valuable.txt");
 
         try{
@@ -141,37 +134,24 @@ public class FloridaInsurance {
         for (var entry:list) {
 
             for(int i = 0; i < sums.size(); i++){
-                if(sums.get(i).county.matches(entry.county)){
-                    sums.get(i).tivDifference += entry.tivDifference;
+                if(sums.get(i).county.matches(entry.county)){//first we iterate through top countries, if the country was already added
+                    sums.get(i).tivDifference = sums.get(i).tivDifference.add(entry.tivDifference);//we just add the tivDifference value
                     added = true;
                 }
             }
-            if(added == false){
-                sums.add(entry);
+            if(added == false){//if it wasn't already added to top countries
+                sums.add(new InsuranceEntry(entry.county, new BigDecimal(entry.tivDifference.toString())));//we add it as new InsuranceEntry
             }
 
             added = false;
-            /*if(top10.size() < 10){
-                top10.add(entry);
-            }
-            else{
-                if(entry.tivDifference > minValue.tivDifference){
-                    top10.remove(minValue);
-                    top10.add(entry);
-                }
-            }
-            deleteDuplicate(top10);
-            minValue = findMin(top10);*/
         }
-
+        //after all entries have been counted we sort the list
         sums.sort(comp);
-
-        //top10.sort(comp);
 
         try{
             out.write("country,value\n".getBytes(StandardCharsets.UTF_8));
         }catch (IOException e){}
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {//and insert the top 10 into the file
             try{
                 out.write(sums.get(i).county.getBytes(StandardCharsets.UTF_8));
                 out.write(",".getBytes(StandardCharsets.UTF_8));
@@ -181,84 +161,40 @@ public class FloridaInsurance {
                 System.out.println("Writing error");
             }
         }
-        //System.out.println("size" + top10.size());
-    }
-
-    public static InsuranceEntry findMin(List<InsuranceEntry> list){
-        InsuranceEntry min;
-
-        min = list.get(0);
-        for(int i = 1; i < list.size(); i++){
-            if(list.get(i).tivDifference < min.tivDifference)
-                min = list.get(i);
-        }
-
-        return min;
-    }
-
-    public static void deleteDuplicate(List<InsuranceEntry> list){
-
-        String tempStr;
-        float tempFl;
-        int i = 0;
-        while(i < list.size()){
-            tempStr = list.get(i).county;
-            tempFl = list.get(i).tivDifference;
-
-            for(int j = i + 1; j < list.size(); j++){
-                if(list.get(j).county.matches(tempStr)){
-                    if(list.get(j).tivDifference <= tempFl){
-                        list.remove(j);
-                    }
-                    else{
-                        if(i >= 0)
-                        list.remove(i);
-                        i--;
-                    }
-                }
-            }
-            i++;
-        }
     }
 
     public static void main(String[] args) {
 
         List<InsuranceEntry> entryList = new ArrayList<>();
 
-        ZipFile zipFile = null;
-        ZipEntry zipEntry = null;
-        InputStream inputStream = null;
-        int entries = 0;
-        int added = 0;
+        ZipFile zipFile;
+        ZipEntry zipEntry;
+        InputStream inputStream;
         String text = null;
 
-        try{
+        try{//opening the zipFile and getting the input stream
             zipFile = new ZipFile("C:\\Users\\Admin\\Desktop\\Studia\\java\\dominikmarchewka\\07-florida-insurance\\FL_insurance.csv.zip");
             zipEntry = zipFile.getEntry("FL_insurance.csv");
             inputStream = zipFile.getInputStream(zipEntry);
 
-            text = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+            text = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);//reading input stream into String
 
         }catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-        var textArray = text.split("\r", 0);
+        var textArray = text.split("\r", 0);//spliting whole input by end of line char
 
-        //System.out.println(textArray[0].split("\n")[0]);
-        entryList.add(new InsuranceEntry(textArray[0].split("\n")[1]));
+        entryList.add(new InsuranceEntry(textArray[0].split("\n")[1]));//first end of line char wass different than the others for some reason
+        //so I split it again and add first entry manually
 
-        for (int i = 1; i < textArray.length; i++) {
-            entries++;
-            added++;
+        for (int i = 1; i < textArray.length; i++) {//add the rest of entries
             entryList.add(new InsuranceEntry(textArray[i]));
         }
 
-
-        System.out.println(entries + " " + added);
-        System.out.println(countCountry(entryList));
-        System.out.println(sumTiv2012(entryList));
-        mostValuable(entryList);
+        System.out.println(countCountry(entryList));//method for count.txt
+        System.out.println(sumTiv2012(entryList));//method for tiv2012.txt
+        mostValuable(entryList);//method for most_valuable.txt
     }
 
 }
--- 07-florida-insurance/src/main/java/uj/java/w7/insurance/InsuranceEntry.java
+++ 07-florida-insurance/src/main/java/uj/java/w7/insurance/InsuranceEntry.java
@@ -1,12 +1,24 @@
 package uj.java.w7.insurance;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 public class InsuranceEntry {
 
     public String policyID;
     public String county;
     public String tiv_2011;
     public String tiv_2012;
-    public float tivDifference;
+    public BigDecimal tivDifference;
+
+    public InsuranceEntry(String county, BigDecimal tivDifference){
+        this.county = county;
+        this.tivDifference = new BigDecimal(tivDifference.toString());
+        this.policyID = null;
+        this.tiv_2011 = null;
+        this.tiv_2012 = null;
+    }
 
     public InsuranceEntry(String line){
         String[] entry = line.split(",");
@@ -16,7 +28,13 @@ public class InsuranceEntry {
         this.tiv_2011 = entry[7];
         this.tiv_2012 = entry[8];
 
-        this.tivDifference = Float.parseFloat(this.tiv_2012) - Float.parseFloat(this.tiv_2011);
+        BigDecimal bd1 = new BigDecimal(this.tiv_2011);
+        BigDecimal bd2 = new BigDecimal(this.tiv_2012);
+        //System.out.println(bd1 + " " + this.tiv_2011);
+        //System.out.println(bd2 + " " + this.tiv_2012);
+
+        this.tivDifference =  new BigDecimal(bd2.subtract(bd1).toString());
+        //System.out.println(this.tivDifference);
     }
 
     public void DisplayEntry(){
