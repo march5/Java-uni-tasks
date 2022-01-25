5f9d7b2914de4d2805634aa3728d27eda6ab632b
Dominik Marchewka <marchewkad09@gmail.com>
2022-01-25 17:50:06 +0100

    calc first commit

---
+++ 09-delegations-calculator/09-delegations-calculator-main/src/main/java/uj/java/pwj2020/delegations/Calc.java
@@ -0,0 +1,14 @@
+package uj.java.pwj2020.delegations;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Calc {
+
+    BigDecimal calculate(String name, String start, String end, BigDecimal dailyRate) {
+        return null;
+    }
+}
